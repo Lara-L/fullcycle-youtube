ca4a5f688b3a3338e7b21d259a27bd95b855cf2a
Lara-L <larasantos.eq@gmail.com>
2024-10-28 21:35:36 -0300

    :sparkles: feat: Saving chunks temporarily

--- src/main/java/com/fullcycle/youtube/fcyoutube/service/VideoService.java
+++ src/main/java/com/fullcycle/youtube/fcyoutube/service/VideoService.java
@@ -2,24 +2,34 @@ package com.fullcycle.youtube.fcyoutube.service;
 
 import java.io.File;
 import java.io.IOException;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-
+@Service
 public class VideoService {
 
   private final String TEMP_DIR = "/temp/videos/";
+  public static final String ROUTING_KEY = "upload.complete";
 
-  public void saveChunk(String fileId, int chunkIndex, MultipartFile file) throws IOException {
-    File tempFile = new File(TEMP_DIR + fileId + "_" + chunkIndex);
-    file.transferTo(tempFile);
+  @Autowired
+  private RabbitTemplate rabbitTemplate;
+
+  public void saveChunk(String fileId, int chunkIndex, MultipartFile file) {
+    try {
+      File tempFile = new File(TEMP_DIR + fileId + "_" + chunkIndex);
+      file.transferTo(tempFile);
+    } catch (IOException e) {
+      throw new RuntimeException("Erro ao salvar o chunk do arquivo" + fileId, e);
+    }
   }
 
   public void publishUploadCompletedEvent(String fileId) {
-    rabbitTemplate.convertAndSend("upload.complete", fileId);
+    rabbitTemplate.convertAndSend(ROUTING_KEY + fileId); //no gpt tava "upload.complete"
   }
 
   public File assembleFile(String fileId, int totalChunks) {
-    return null;
+    return new File(TEMP_DIR + fileId);
   }
 }
